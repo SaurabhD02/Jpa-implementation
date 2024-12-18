6152174c039dd320a70187881b8db7b9b3dca257
rf-saurabhd <saurabhdorle20598@gmail.com>
2024-12-18 11:55:07 +0530

    changes configuration

--- src/main/java/com/spd/jpademo/entity/Address.java
+++ src/main/java/com/spd/jpademo/entity/Address.java
@@ -1,6 +1,9 @@
 package com.spd.jpademo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Address {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
 	private String streetName;
